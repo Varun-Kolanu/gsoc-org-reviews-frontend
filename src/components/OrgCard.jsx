9e1bcc8d8ed017f9c21e93964c5ace59962a2988
Sagnik Mandal <acriticalcynic@outlook.com>
2024-12-18 22:51:07 +0530

    added logo background colors

--- src/components/OrgCard.jsx
+++ src/components/OrgCard.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
-const OrgCard = ({ image_url, name }) => {
+const OrgCard = ({ image_url, name, image_background_color }) => {
     return (
         <a href={name} target='_blank'>
             <div className='w-60 h-72 overflow-hidden p-8 border rounded-md hover:shadow-xl flex flex-col justify-evenly items-center hover:bg-gray-50 transition-all relative'>
                 <div className='w-40 h-40 relative'>
-                    <img src={image_url} alt={name} className='w-full h-full object-contain object-center' />
+                    <div className='w-full h-full' style={{ backgroundColor: image_background_color }}>
+                        <img src={image_url} alt={name} className='w-full h-full object-contain object-center' />
+                    </div>
                 </div>
                 <h3 className='text-gray-600 text-lg font-galano text-center'>{name}</h3>
             </div>
