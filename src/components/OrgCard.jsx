579096cb5cd6860221f0ca2894a2340fc51b4224
Varun-Kolanu <kolanuvarun739@gmail.com>
2024-12-25 02:24:56 +0530

    Sort orgs based on rating and review

--- src/components/OrgCard.jsx
+++ src/components/OrgCard.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
+import { pluralized } from '../utils/utils'
 
-const OrgCard = ({ image_url, name, image_background_color }) => {
+const OrgCard = ({ image_url, name, image_background_color, reviewCount, avgRating }) => {
     return (
         <a href={name} target='_blank'>
-            <div className='w-60 h-72 overflow-hidden p-8 border rounded-md hover:shadow-xl flex flex-col justify-evenly items-center hover:bg-gray-50 transition-all relative'>
+            <div className='w-60 h-80 overflow-hidden p-8 border rounded-md hover:shadow-xl flex flex-col justify-evenly items-center hover:bg-gray-50 transition-all relative'>
                 <div className='w-40 h-40 relative'>
                     <div className='w-full h-full' style={{ backgroundColor: image_background_color }}>
                         <img src={image_url} alt={name} className='w-full h-full object-contain object-center' />
                     </div>
                 </div>
                 <h3 className='text-gray-600 text-lg font-galano text-center'>{name}</h3>
+                <div className='text-orange-500 font-bold flex items-center gap-1'> {parseFloat(avgRating.toFixed(2))} <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    width="24"
+                    height="24"
+                    className="w-6 h-6 fill-orange-500 inline-block"
+                >
+                    <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
+                </svg></div>
+                <div className='text-gray-500'>
+                    ( {reviewCount} review{pluralized(reviewCount)} )
+                </div>
             </div>
         </a>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,52 @@
 import React, { useEffect, useMemo, useState } from 'react'
-import { useLocation, useNavigate } from 'react-router-dom';
 import axios from '../utils/axiosInstance';
 import OrgCard from '../components/OrgCard';
 import Loader from '../components/Loader';
 import Search from '../components/Search';
 import debounce from 'lodash.debounce';
 import { pluralized } from '../utils/utils';
+import up_arrow from "../assets/up_arrow.svg"
+import down_arrow from "../assets/down_arrow.svg"
 
 const Home = () => {
     const [orgs, setOrgs] = useState([]);
     const [loading, setLoading] = useState(false);
     const [search, setSearch] = useState("");
     const [filteredOrgs, setFilteredOrgs] = useState([]);
+    const [sortByRating, setSortByRating] = useState("");
+    const [sortByReviews, setSortByReviews] = useState("");
+
+    const handleReviewSort = () => {
+        if (sortByReviews === "desc") {
+            setSortByReviews("asc");
+        } else if (sortByReviews === "asc") {
+            setSortByReviews("");
+        } else {
+            setSortByReviews("desc");
+        }
+    }
+
+    const handleRatingSort = () => {
+        if (sortByRating === "desc") {
+            setSortByRating("asc");
+        } else if (sortByRating === "asc") {
+            setSortByRating("");
+        } else {
+            setSortByRating("desc");
+        }
+    }
+
+    const cmpRating = (a, b) => {
+        if (sortByRating === "desc") return b.avgRating - a.avgRating;
+        if (sortByRating === "asc") return a.avgRating - b.avgRating;
+        return 0;
+    }
+
+    const cmpReviews = (a, b) => {
+        if (sortByReviews === "desc") return b.reviewCount - a.reviewCount;
+        if (sortByReviews === "asc") return a.reviewCount - b.reviewCount;
+        return 0;
+    }
 
     const debouncedHandleSearch = useMemo(() => {
         return debounce((value) => {
@@ -46,9 +81,14 @@ const Home = () => {
                 <button className='text-white rounded-md bg-orange-500 p-2 font-semibold cursor-default'>
                     {filteredOrgs.length} result{pluralized(filteredOrgs.length)}
                 </button>
+                <div className='flex gap-4 items-center'>
+                    <span className='font-bold'>Sort by:</span>
+                    <button className='flex items-center text-sm font-semibold px-3 py-2 bg-blue-500 text-white rounded-lg gap-2' onClick={handleRatingSort}> {sortByRating === "asc" && <img src={up_arrow} alt="(Asc)" />} {sortByRating === "desc" && <img src={down_arrow} alt="(Desc)" />} Rating</button>
+                    <button className='flex items-center text-sm font-semibold px-3 py-2 bg-gray-500 text-white rounded-lg gap-2' onClick={handleReviewSort}> {sortByReviews === "asc" && <img src={up_arrow} alt="(Asc)" />} {sortByReviews === "desc" && <img src={down_arrow} alt="(Desc)" />} Reviews</button>
+                </div>
                 <div className='flex gap-10 flex-wrap justify-center'>
                     {
-                        filteredOrgs.map((org) => (
+                        filteredOrgs.sort(cmpRating).sort(cmpReviews).map((org) => (
                             <div key={org.name} >
                                 <OrgCard {...org} />
                             </div>
